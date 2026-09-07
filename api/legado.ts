8d473e29e95038e4e34ac2edc676a85236220d7d
gugaon0210tv <gugaon0210@daum.net>
2026-09-07 18:47:01 +0900

    Update legado.ts

--- api/legado.ts
+++ api/legado.ts
@@ -1,55 +1,82 @@
 import { Request, Response } from 'express'
 import { FORMAT_CONTENT_TYPE } from '../service/edge'
+
+interface LegadoData {
+  name: string
+  contentType: string
+  id: number
+  loginCheckJs: string
+  loginUi: string
+  loginUrl: string
+  header: string
+  url: string
+}
+
 module.exports = async (request: Request, response: Response) => {
   console.log('Import url: ' + request.url)
-  let api = request.query['api']
-  let name = request.query['name'] ?? '大声朗读'
-  let voiceName = request.query['voiceName'] ?? 'zh-CN-XiaoxiaoNeural'
-  let styleName = request.query['styleName']
-  let styleDegree = request.query['styleDegree']
-  let voiceFormat = request.query['voiceFormat']
-  let lexicon = request.query['lexicon'] ?? ''
-  let token = request.query['token'] ?? ''
+
+  const api = request.query['api']
+  const name = request.query['name'] ?? '大声朗读'
+  const voiceName = request.query['voiceName'] ?? 'zh-CN-XiaoxiaoNeural'
+  const styleName = request.query['styleName']
+  const styleDegree = request.query['styleDegree']
+  const voiceFormat = request.query['voiceFormat']
+  const lexicon = request.query['lexicon'] ?? ''
+  const token = request.query['token'] ?? ''
 
   if (Array.isArray(voiceFormat)) {
-    throw `Invalid format ${voiceFormat}`
-  }
-  if (!FORMAT_CONTENT_TYPE.has(voiceFormat as string)) {
-    throw `Invalid format ${voiceFormat}`
+    throw new Error(`Invalid format ${voiceFormat}`)
   }
 
-  const data = {}
-  data['name'] = name == '' ? 'TTS' : name
-  data['contentType'] = FORMAT_CONTENT_TYPE.get(voiceFormat as string)
-  data['id'] = Date.now()
-  data['loginCheckJs'] = ''
-  data['loginUi'] = ''
-  data['loginUrl'] = ''
+  if (!voiceFormat) {
+    throw new Error('Invalid format')
+  }
 
-  let header = {
+  const contentType = FORMAT_CONTENT_TYPE.get(voiceFormat)
+
+  if (!contentType) {
+    throw new Error(`Invalid format ${voiceFormat}`)
+  }
+
+  const data: LegadoData = {
+    name: name === '' ? 'TTS' : String(name),
+    contentType,
+    id: Date.now(),
+    loginCheckJs: '',
+    loginUi: '',
+    loginUrl: '',
+    header: '',
+    url: '',
+  }
+
+  const header = {
     'Content-Type': 'text/plain',
-    Authorization: 'Bearer ' + token,
+    Authorization: 'Bearer ' + String(token),
     Format: voiceFormat,
   }
-  data['header'] = JSON.stringify(header)
-  let ssml =
+
+  data.header = JSON.stringify(header)
+
+  const ssml =
     `<speak xmlns="http://www.w3.org/2001/10/synthesis" xmlns:mstts="http://www.w3.org/2001/mstts" xmlns:emo="http://www.w3.org/2009/10/emotionml" version="1.0" xml:lang="en-US">` +
-    `<voice name="${voiceName}">` +
-    (lexicon === '' ? '' : `<lexicon uri="${lexicon}"/>`) +
+    `<voice name="${String(voiceName)}">` +
+    (lexicon === '' ? '' : `<lexicon uri="${String(lexicon)}"/>`) +
     (styleName
-      ? `<mstts:express-as style="${styleName}" styledegree="${styleDegree}">`
-      : ``) +
+      ? `<mstts:express-as style="${String(styleName)}" styledegree="${String(styleDegree ?? '')}">`
+      : '') +
     `<prosody rate="{{(speakSpeed - 10) * 2}}%" pitch="+0Hz">` +
     `{{String(speakText).replace(/&/g, '&amp;').replace(/\"/g, '&quot;').replace(/'/g, '&apos;').replace(/</g, '&lt;').replace(/>/g, '&gt;')}}` +
     `</prosody>` +
-    (styleName ? ` </mstts:express-as>` : ``) +
+    (styleName ? ` </mstts:express-as>` : '') +
     `</voice>` +
     `</speak>`
-  let body = {
+
+  const body = {
     method: 'POST',
     body: ssml,
   }
 
-  data['url'] = api + ',' + JSON.stringify(body)
+  data.url = String(api ?? '') + ',' + JSON.stringify(body)
+
   response.status(200).json(data)
 }
