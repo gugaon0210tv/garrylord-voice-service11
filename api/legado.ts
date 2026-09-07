4e8d6b2542f87e0171275cf2353a0f38a53b90e5
gugaon0210tv <gugaon0210@daum.net>
2026-09-07 18:57:47 +0900

    Update legado.ts

--- api/legado.ts
+++ api/legado.ts
@@ -1,17 +1,6 @@
 import { Request, Response } from 'express'
 import { FORMAT_CONTENT_TYPE } from '../service/edge'
 
-interface LegadoData {
-  name: string
-  contentType: string
-  id: number
-  loginCheckJs: string
-  loginUi: string
-  loginUrl: string
-  header: string
-  url: string
-}
-
 module.exports = async (request: Request, response: Response) => {
   console.log('Import url: ' + request.url)
 
@@ -20,58 +9,36 @@ module.exports = async (request: Request, response: Response) => {
   const voiceName = request.query['voiceName'] ?? 'zh-CN-XiaoxiaoNeural'
   const styleName = request.query['styleName']
   const styleDegree = request.query['styleDegree']
+
+  const voiceFormatQuery = request.query['voiceFormat']
+
+  if (typeof voiceFormatQuery !== 'string') {
+    throw new Error(
+      `Invalid format ${String(voiceFormatQuery)}`,
+    )
+  }
+
+  const voiceFormat = voiceFormatQuery
+
   const lexicon = request.query['lexicon'] ?? ''
   const token = request.query['token'] ?? ''
 
-  if (Array.isArray(voiceFormat)) {
+  if (!FORMAT_CONTENT_TYPE.has(voiceFormat)) {
     throw new Error(`Invalid format ${voiceFormat}`)
   }
 
-  if (!voiceFormat) {
-    throw new Error('Invalid format')
-  }
+  const data: Record<string, unknown> = {}
 
-const voiceFormatQuery = request.query['voiceFormat']
+  data['name'] =
+    name === '' ? 'TTS' : name
 
-if (typeof voiceFormatQuery !== 'string') {
-  throw new Error(`Invalid format ${String(voiceFormatQuery)}`)
-}
+  data['contentType'] =
+    FORMAT_CONTENT_TYPE.get(voiceFormat)
 
-const voiceFormat = voiceFormatQuery
-
-if (!FORMAT_CONTENT_TYPE.has(voiceFormat)) {
-  throw new Error(`Invalid format ${voiceFormat}`)
-}
-
-if (
-  typeof voiceFormatQuery !== 'string' ||
-  voiceFormatQuery.length === 0
-) {
-  throw new Error(`Invalid format ${String(voiceFormatQuery)}`)
-}
-
-const voiceFormat = voiceFormatQuery
-
-const contentType = FORMAT_CONTENT_TYPE.get(voiceFormat)
-
-if (!contentType) {
-  throw new Error(`Invalid format ${voiceFormat}`)
-}
-
-  if (!contentType) {
-    throw new Error(`Invalid format ${voiceFormat}`)
-  }
-
-  const data: LegadoData = {
-    name: name === '' ? 'TTS' : String(name),
-    contentType,
-    id: Date.now(),
-    loginCheckJs: '',
-    loginUi: '',
-    loginUrl: '',
-    header: '',
-    url: '',
-  }
+  data['id'] = Date.now()
+  data['loginCheckJs'] = ''
+  data['loginUi'] = ''
+  data['loginUrl'] = ''
 
   const header = {
     'Content-Type': 'text/plain',
@@ -79,19 +46,28 @@ if (!contentType) {
     Format: voiceFormat,
   }
 
-  data.header = JSON.stringify(header)
+  data['header'] =
+    JSON.stringify(header)
 
   const ssml =
-    `<speak xmlns="http://www.w3.org/2001/10/synthesis" xmlns:mstts="http://www.w3.org/2001/mstts" xmlns:emo="http://www.w3.org/2009/10/emotionml" version="1.0" xml:lang="en-US">` +
+    `<speak xmlns="http://www.w3.org/2001/10/synthesis" ` +
+    `xmlns:mstts="http://www.w3.org/2001/mstts" ` +
+    `xmlns:emo="http://www.w3.org/2009/10/emotionml" ` +
+    `version="1.0" xml:lang="en-US">` +
     `<voice name="${String(voiceName)}">` +
-    (lexicon === '' ? '' : `<lexicon uri="${String(lexicon)}"/>`) +
+    (lexicon === ''
+      ? ''
+      : `<lexicon uri="${String(lexicon)}"/>`) +
     (styleName
-      ? `<mstts:express-as style="${String(styleName)}" styledegree="${String(styleDegree ?? '')}">`
+      ? `<mstts:express-as style="${String(styleName)}" ` +
+        `styledegree="${String(styleDegree ?? '')}">`
       : '') +
     `<prosody rate="{{(speakSpeed - 10) * 2}}%" pitch="+0Hz">` +
-    `{{String(speakText).replace(/&/g, '&amp;').replace(/\"/g, '&quot;').replace(/'/g, '&apos;').replace(/</g, '&lt;').replace(/>/g, '&gt;')}}` +
+    `{{String(speakText).replace(/&/g, '&amp;').replace(/"/g, '&quot;').replace(/'/g, '&apos;').replace(/</g, '&lt;').replace(/>/g, '&gt;')}}` +
     `</prosody>` +
-    (styleName ? ` </mstts:express-as>` : '') +
+    (styleName
+      ? ` </mstts:express-as>`
+      : '') +
     `</voice>` +
     `</speak>`
 
@@ -100,7 +76,8 @@ if (!contentType) {
     body: ssml,
   }
 
-  data.url = String(api ?? '') + ',' + JSON.stringify(body)
+  data['url'] =
+    String(api) + ',' + JSON.stringify(body)
 
   response.status(200).json(data)
 }
