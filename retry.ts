2e5717965fc6452e9891aff77d2b5fb8552983b3
gugaon0210tv <gugaon0210@daum.net>
2026-09-07 18:47:16 +0900

    Update retry.ts

--- retry.ts
+++ retry.ts
@@ -1,24 +1,32 @@
 type Fn = () => Promise<any>
-type ErrorFn = (index, error) => void
+
+type ErrorFn = (index: number, error: unknown) => void
+
 export const retry = async function (
   fn: Fn,
   times: number,
   errorFn?: ErrorFn,
   failedMessage?: string,
 ) {
-  let reason = {
+  const reason: {
+    message: string
+    errors: unknown[]
+  } = {
     message: failedMessage ?? '多次尝试后失败',
     errors: [],
   }
+
   for (let i = 0; i < times; i++) {
     try {
       return await fn()
-    } catch (error) {
+    } catch (error: unknown) {
       if (errorFn) {
         errorFn(i, error)
       }
+
       reason.errors.push(error)
     }
   }
+
   throw reason
 }
